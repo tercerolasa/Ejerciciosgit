c68466de8a268f8473cb0c7470c12a81ea0074dc
alejandro <tercerolasa@gmail.com>
2021-02-22 10:02:11 +0100

    1 commit new java

---
+++ ejerciciosgit/src/HelloGitHub/contador.java
@@ -0,0 +1,12 @@
+package HelloGitHub;
+
+public class contador {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			for(int i = 0; i<=5;i++) {
+				System.out.print(i+" ");
+			}
+	}
+
+}
